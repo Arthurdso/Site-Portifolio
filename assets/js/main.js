7acd792a85f92aa1f5b373d8a4516a553a8af57e
Arthurdso <arthurdso96@gmail.com>
2022-02-15 18:51:59 -0300

    Melhorando codigo, tema escuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,27 @@
 const trocaTemaEscuro = document.querySelector(".theme");
-const temaEscuro = document.querySelector(".escuro");
-const temaEscuro2 = document.querySelector(".escuro2")
+const temaEscuro = document.querySelectorAll(".escuro");
 const trocaTemaClaro = document.querySelector(".temaClaro");
+const icones = document.querySelectorAll(".icones")
 
-trocaTemaEscuro.addEventListener('click', () => {
-    console.log('cliquei');
-    temaEscuro.classList.toggle('bg-dark')
-    temaEscuro.classList.add('bg-light')
-    temaEscuro2.classList.toggle('bg-dark')
-    temaEscuro2.classList.add('bg-light')
-    trocaTemaEscuro.classList.add('temaClaro')
-});
+// trocaTemaEscuro.addEventListener('click', () => {
+//     console.log('cliquei');
+//     temaEscuro.classList.toggle('bg-dark')
+//     temaEscuro.classList.add('bg-light')
+//     temaEscuro2.classList.toggle('bg-dark')
+//     temaEscuro2.classList.add('bg-light')
+// });
+
+for (let i = 0; i < temaEscuro.length; i++) {
+    trocaTemaEscuro.addEventListener("click", function() {
+    temaEscuro[i].classList.toggle("bg-dark");
+    temaEscuro[i].classList.add('bg-light')
+    });
+}
+
+for (let i = 0; i < icones.length; i++) {
+    trocaTemaEscuro.addEventListener("click", function() {
+    icones[i].classList.toggle("textoPreto");
+    icones[i].classList.add('textoBranco')
+
+    });
+}
